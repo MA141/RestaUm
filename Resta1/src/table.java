3f31dace15c69e4431ddf466c621db50e60263aa
MA141 <63609160+MA141@users.noreply.github.com>
2020-04-13 16:10:24 -0300

    Modifica a classe table
    Adicionado o método create

--- Resta1/src/table.java
+++ Resta1/src/table.java
@@ -4,6 +4,27 @@ public class table {
 	int cont=7;
 	
 	
+	void create() {
+		for (int i=0; i<7; i++) {
+			for(int j=0; j<7; j++) {
+				if (i>=2 && i<=4) {
+					tabela[i][j].valid=true;
+					tabela[i][j].preenchido=true;
+				}
+				else {
+					if(j>=2 && j<=4) {
+						tabela[i][j].valid=true;
+						tabela[i][j].preenchido=true;
+					}
+					else {
+						tabela[i][j].valid=false;
+						tabela[i][j].preenchido=false;
+					}
+				}
+			}
+		}
+	}
+	
 	void mostra() {
 		for (int i=0; i<7; i++) {
 			System.out.print(cont+" ");
@@ -31,11 +52,25 @@ public class table {
 		///Pega duas posicoes, verifica se a pos inicial e a final sao validas (a inicial tem peça e a final não, e se no meio delas tem peça) e depois tira da inicial, coloca na final, e tira a do meio.
 	}
 	
+	boolean isValid(String com) { //com=command
+		/*If comando valido, i.e.:*/
+		/*Pos inicial preenchida, final vazia, do meio preenchida, distancia de 1 casa*/
+		if(tabela[int(com[0]-98)][int(com[1])].valid && tabela[int(com[0]-98)][int(com[1])].preenchido && tabela[int(com[3]-98)][int(com[4])].valid && tabela[int(com[3]-98)][int(com[4])]..preenchido==false) {
+			if /*posicao do meio.preenchida==true e a distancia eh 1.*/
+		}
+		/*Pos eh acessada vendo as posicoes de com, tipo, tabela[int(com[0])-98][int(com[1]])*/
+		
+				return true;
+			/*else*/
+				return false;
+		/*else*/
+		return false;
+	}
+	
 	
 	public static void main(String[] args) {
 		table tab=new table();
-		tab.mostra();
-		
+		tab.mostra();	
 	}
 
 }
